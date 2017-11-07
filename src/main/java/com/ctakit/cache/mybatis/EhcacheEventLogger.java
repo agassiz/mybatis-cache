8f1df54060f97b71bda248c8514c80712a99690b
lizhenmin <andyhome163@163.com>
2017-11-07 10:20:15 +0800

    缓存级别日志修改  ,logback配置 <logger name="com.ctakit.cache.mybatis.EhcacheEventLogger" level="DEBUG" />

--- src/main/java/com/ctakit/cache/mybatis/EhcacheEventLogger.java
+++ src/main/java/com/ctakit/cache/mybatis/EhcacheEventLogger.java
@@ -1,4 +1,5 @@
 package com.ctakit.cache.mybatis;
+
 import org.ehcache.event.CacheEvent;
 import org.ehcache.event.CacheEventListener;
 import org.slf4j.Logger;
@@ -8,13 +9,20 @@ import org.slf4j.LoggerFactory;
  * Created by lujun.chen on 2017/6/21.
  */
 public class EhcacheEventLogger implements CacheEventListener<Object, Object> {
-  private static final Logger LOGGER = LoggerFactory.getLogger(EhcacheEventLogger.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(EhcacheEventLogger.class);
 
-  public EhcacheEventLogger() {
-	  LOGGER.info("init EhcacheEventLogger");
-}
-  @Override
-  public void onEvent(CacheEvent<?, ?> event) {
-    LOGGER.info("Event: " + event.getType() + " Key: " + event.getKey() + " old value: " + event.getOldValue() + " new value: " + event.getNewValue());
-  }
+	public EhcacheEventLogger() {
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.info("init EhcacheEventLogger");
+		}
+	}
+
+	@Override
+	public void onEvent(CacheEvent<?, ?> event) {
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Event: " + event.getType() + " Key: " + event.getKey() + " old value: " + event.getOldValue()
+					+ " new value: " + event.getNewValue());
+		}
+
+	}
 }
