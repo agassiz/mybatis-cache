5d2f9062eea684c5e46039dfd459290a69b410a1
lizhenmin <andyhome163@163.com>
2017-11-07 11:14:46 +0800

    去除无用日志

--- src/main/java/com/ctakit/cache/mybatis/EhcacheEventLogger.java
+++ src/main/java/com/ctakit/cache/mybatis/EhcacheEventLogger.java
@@ -11,11 +11,6 @@ import org.slf4j.LoggerFactory;
 public class EhcacheEventLogger implements CacheEventListener<Object, Object> {
 	private static final Logger LOGGER = LoggerFactory.getLogger(EhcacheEventLogger.class);
 
-	public EhcacheEventLogger() {
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("init EhcacheEventLogger");
-		}
-	}
 
 	@Override
 	public void onEvent(CacheEvent<?, ?> event) {
